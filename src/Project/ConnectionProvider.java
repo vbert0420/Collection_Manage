178e74546caf4d71dac0d99999d172f0cc3ffefa
john123-coder <johnnicabenero.ccs@sccpag.edu.ph>
2023-05-25 07:45:20 +0800

    Capstone Project

---
+++ src/Project/ConnectionProvider.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+import java.sql.*;
+/**
+ *
+ * @author johnn
+ */
+public class ConnectionProvider {
+    public static Connection getcon()
+    {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/payor","root","123456");
+            return con;
+        }
+        catch(Exception e)
+        {
+            return null;
+        }
+    }
+    
+}
